a5d406e1f6f88e7d00107031510a7452f2343e33
PEACH\hausw <hausweigerfelix@gmail.com>
2021-08-07 20:51:20 +0200

    Add project files.

---
+++ DTO/MealDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinnerTime.DTO
+{
+    public class MealDTO
+    {
+
+        public int ID { get; set; }
+        public String MealType { get; set; }
+
+        public MealDTO(int id, String mt) {
+            ID = id;
+            MealType = mt;
+        }
+    }
+}
---
+++ Database/dbController.cs
@@ -0,0 +1,59 @@
+﻿using DinnerTime.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinnerTime.Database
+{
+    public static class dbController
+    {
+
+        public static SqlConnection connection = new SqlConnection();
+
+        public static void connectDB() {
+            string cn_String = Properties.Settings.Default.ConnectionString;
+
+            try
+            {
+                dbController.connection = new SqlConnection(cn_String);
+                dbController.connection.Open();
+                Console.WriteLine("Connection was successful!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Connection Error!");
+                throw;
+            }
+        }
+
+        public static List<MealDTO> getMealTypes() {
+
+            List<MealDTO> mealHolder = new List<MealDTO>();
+
+            try
+            {
+                string readString = "select * from [Mealtype]";
+                using (SqlCommand command = new SqlCommand(readString, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            MealDTO meal = new MealDTO(int.Parse(reader["ID"].ToString()), reader["MealType"].ToString());
+                            mealHolder.Add(meal);
+                        }
+                    }
+                }
+                return mealHolder;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+    }
+}
