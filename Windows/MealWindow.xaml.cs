8536b801cea72b6b4d9b48b2f9f67e61d69fc2cf
PEACH\hausw <hausweigerfelix@gmail.com>
2021-08-28 19:50:05 +0200

    filters are working and now also we have a great template with no functions to edit, remove and add

--- Windows/MealWindow.xaml.cs
+++ Windows/MealWindow.xaml.cs
@@ -19,13 +19,30 @@ namespace DinnerTime.Windows
     public partial class MealWindow : Window
     {
         public List<FoodDTO> FoodList { get; set; }
+        public List<FoodDTO> FilteredFoodList { get; set; }
 
         public MealWindow()
         {
             InitializeComponent();
             FoodList = new List<FoodDTO>();
             FoodList = dbController.getEveryFood();
+            FilterFood();
             DataContext = this;
+        }
+
+
+        private List<FoodDTO> FilterFood()
+        {
+            FilteredFoodList = new List<FoodDTO>();
+
+            foreach (var food in FoodList)
+            {
+                if (food.MealTypeID == SelectedDTOs.Selected.SelectedMeal.ID) {
+                    FilteredFoodList.Add(food);
+                }
+            }
+
+            return FilteredFoodList;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -38,6 +55,17 @@ namespace DinnerTime.Windows
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 DragMove();
+            }
+        }
+
+
+        private void MealSelectionChange(object sender, SelectionChangedEventArgs e)
+        {
+            if (FilteredMealListSelection.SelectedItem != null) {
+                FoodDTO meal = (FoodDTO)FilteredMealListSelection.SelectedItem;
+                MessageBox.Show("Name: " + meal.MealName + "\n" + 
+                                "Description: " + meal.MealDescription + "\n" +
+                                "Materials: " + meal.MealMaterial);
             }
         }
     }
